715b4e5a3c03957f4def805532b889ebddf8aa9f
mahesh <patilm006@gmail.com>
2020-07-08 19:41:02 +0530

    javascript commit

---
+++ Javascript/Lab1/lab1_4.js
@@ -0,0 +1,10 @@
+
+var msg;
+msg = "<p><code>The actual script is in external script file called common.js</code></p>";
+
+function addNos(headVar, bodyVar) {
+
+    return parseInt(headVar) + parseInt(bodyVar);
+
+
+}
